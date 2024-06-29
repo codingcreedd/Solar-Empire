188fb8b6e39cebfb17cc9a665e6814afa740c369
codingcreedd <moubayedmarwancoding@gmail.com>
2024-06-29 14:55:35 +0300

    Add responsivity to hero section

--- src/Components/Hero.jsx
+++ src/Components/Hero.jsx
@@ -8,8 +8,8 @@ const Hero = () => {
     <div className="flex flex-col">
       <img src="https://i.ibb.co/59jJ31v/hero-Banner.jpg" alt="hero-Banner" border="0" className="w-full" />
       <div className="text-center flex flex-col mt-10 items-center gap-6">
-        <h1 className="text-4xl font-bold text-green-600">Welcome to Solar Empire <sup className="text-md">TM</sup></h1>
-        <p className="max-w-[50%]">Our mission is to empower your home with reliable and sustainable energy solutions, alleviating 
+        <h1 className="text-4xl font-bold text-green-600 max-md:text-2xl">Welcome to Solar Empire <sup className="text-md max-md:text-sm">TM</sup></h1>
+        <p className="md:max-w-[50%] max-md:w-[90%]">Our mission is to empower your home with reliable and sustainable energy solutions, alleviating 
           the frustration of electricity shortages in Lebanon. Our innovative solar power fans are designed 
           to provide a breath of fresh air, literally! By harnessing the power of the sun, our fans ensure a 
           consistent and eco-friendly cooling experience, while reducing your reliance on the grid.</p>
@@ -18,7 +18,7 @@ const Hero = () => {
       <Product img="https://i.ibb.co/VST6MdH/Blue-Black-Minimalist-Modern-New-Collection-Backpack-Promotion-Instagram-Post-3.png" name="Solar Empire's White / Black Solar Fan"
       price={42} lastPrice={75} title="Top Product"/>
 
-      <h1 className="border bg-green-600 text-white border-green-600 py-4 mx-10 rounded-xl text-4xl text-center mt-20 font-bold mb-20 hover:bg-green-600 hover:text-black transition-all hover:-translate-y-4">All Products</h1>
+      <h1 className="border bg-green-600 text-white border-green-600 py-4 mx-10 max-md:py-2 max-md:mx-4 max-md:text-2xl max-md:mt-6 max-md:mb-10 rounded-xl text-4xl text-center mt-20 font-bold mb-20 hover:bg-green-600 hover:text-black transition-all hover:-translate-y-4">All Products</h1>
       <div className="flex flex-wrap justify-center items-center gap-4">
         {
           products.map(product => (
--- src/Components/Product/Product.jsx
+++ src/Components/Product/Product.jsx
@@ -6,11 +6,11 @@ export const Element = ({title, children}) => {
     const [isOpen, setIsOpen] = useState(false);
 
     return (
-        <div className="flex flex-col gap-5 cursor-pointer shadow-2xl px-4 py-4 rounded-lg" onClick={() => {setIsOpen(!isOpen)}}>
+        <div className="flex flex-col gap-5 max-md:gap-3 cursor-pointer shadow-2xl px-4 py-4 rounded-lg" onClick={() => {setIsOpen(!isOpen)}}>
             <div className="flex justify-between">
-                <p className="text-green-600 text-xl font-bold">{title}</p>
+                <p className="text-green-600 md:text-xl font-bold">{title}</p>
                 {
-                    !isOpen ? <i className='bx bx-plus text-2xl'></i> : <i className='bx bx-minus text-2xl' ></i>
+                    !isOpen ? <i className='bx bx-plus md:text-2xl'></i> : <i className='bx bx-minus md:text-2xl' ></i>
                 }
             </div>
 
@@ -34,65 +34,57 @@ const Product = ({img, name, price, title, lastPrice}) => {
     <div className="flex flex-col gap-10">
         {
             title ? (
-                <h1 className="border bg-green-600 text-white border-green-600 py-4 mx-10 rounded-xl text-4xl text-center mt-10 font-bold mb-10 hover:bg-green-600 hover:text-black transition-all hover:-translate-y-4">{title}</h1>
+                <h1 className="border bg-green-600 text-white border-green-600 py-4 mx-10 max-md:py-2 max-md:mx-4 max-md:text-2xl max-md:mt-6 max-md:mb-2 rounded-xl text-4xl text-center mt-10 font-bold mb-10 hover:bg-green-600 hover:text-black transition-all hover:-translate-y-4">{title}</h1>
             ) : 
             null
         }
-        <div className="flex justify-center gap-20">
-            <div className="flex flex-col rounded-lg bg-white">
-            <img src={img} alt="Blue-Black-Minimalist-Modern-New-Collection-Backpack-Promotion-Instagram-Post-3" border="0" className="w-[600px] rounded-lg" />
-                <div className="mt-6 flex gap-4 items-center justify-center">
-                    <div className={`${(imgNum === 1) ? 'w-[20px]' : 'w-[6px]'} h-[6px] rounded-full cursor-pointer ${(imgNum === 1) ? 'bg-green-600' : 'bg-gray-400'}`} onClick={() => {setImgNum(1)}}></div>
-                    <div className={`${(imgNum === 2) ? 'w-[20px]' : 'w-[6px]'} h-[6px] rounded-full cursor-pointer ${(imgNum === 2) ? 'bg-green-600' : 'bg-gray-400'}`} onClick={() => {setImgNum(2)}}></div>
-                    <div className={`${(imgNum === 3) ? 'w-[20px]' : 'w-[6px]'} h-[6px] rounded-full cursor-pointer ${(imgNum === 3) ? 'bg-green-600' : 'bg-gray-400'}`} onClick={() => {setImgNum(3)}}></div>
-                    <div className={`${(imgNum === 4) ? 'w-[20px]' : 'w-[6px]'} h-[6px] rounded-full cursor-pointer ${(imgNum === 4) ? 'bg-green-600' : 'bg-gray-400'}`} onClick={() => {setImgNum(4)}}></div>
-                </div>
-            </div>
-
-            <div className="flex flex-col w-[40%]">
-                <h1 className="text-4xl font-bold max-w-[70%]">{name}</h1>
-                <p className="text-lg mt-3 text-gray-400 font-bold">Feel the Fresh Air without electricity!</p>
+        <div className="flex justify-center gap-20 max-md:gap-6 max-md:flex-col max-md:mx-4 max-md:items-center">
+            <img src={img} alt="Blue-Black-Minimalist-Modern-New-Collection-Backpack-Promotion-Instagram-Post-3" border="0" className="w-[600px] rounded-lg max-md:w-[90%]" />
+            
+            <div className="flex flex-col w-[40%] max-md:w-full">
+                <h1 className="text-4xl font-bold md:max-w-[70%] max-md:w-full max-md:text-2xl">{name}</h1>
+                <p className="text-lg mt-3 text-gray-400 font-bold max-md:text-sm">Feel the Fresh Air without electricity!</p>
 
                 <div className="flex items-center gap-3 mt-4">
-                    <p className="font-bold text-green-600 text-lg">${price}</p>
-                    <p className="text-gray-500 font-bold text-lg line-through">${lastPrice}</p>
+                    <p className="font-bold text-green-600 text-lg max-md:text-sm">${price}</p>
+                    <p className="text-gray-500 font-bold text-lg line-through max-md:text-sm">${lastPrice}</p>
                     <div className="text-center text-white bg-green-600 rounded-lg px-2 font-bold text-sm">
                         Free Delivery
                     </div>
                 </div>
 
-                <div className="flex gap-5 items-center mt-5">
-                    <i className='bx bxs-heart text-2xl text-green-600'></i>
-                    <p>Good Night Sleep</p>
-                    <i className='bx bxs-heart text-2xl text-green-600'></i>
-                    <p>4 Speeds</p>
-                    <i className='bx bxs-heart text-2xl text-green-600'></i>
-                    <p>USB Port</p>
+                <div className="flex gap-5 items-center mt-5 max-md:gap-2">
+                    <i className='bx bxs-heart text-2xl text-green-600 max-md:text-sm'></i>
+                    <p className="max-md:text-sm">Good Night Sleep</p>
+                    <i className='bx bxs-heart text-2xl text-green-600 max-md:text-sm'></i>
+                    <p  className="max-md:text-sm">4 Speeds</p>
+                    <i className='bx bxs-heart text-2xl text-green-600 max-md:text-sm'></i>
+                    <p className="max-md:text-sm">USB Port</p>
                 </div>
 
                 <div className="flex gap-5 items-center">
-                    <i className='bx bxs-heart text-2xl text-green-600'></i>
-                    <p>LED Light</p>
-                    <i className='bx bxs-heart text-2xl text-green-600'></i>
-                    <p>Free Delivery</p>
+                    <i className='bx bxs-heart text-2xl text-green-600 max-md:text-sm'></i>
+                    <p className="max-md:text-sm">LED Light</p>
+                    <i className='bx bxs-heart text-2xl text-green-600 max-md:text-sm'></i>
+                    <p className="max-md:text-sm">Free Delivery</p>
                 </div>
                 
                 <AddToCartBtn />
-                <p className="font-bold mt-4 text-center">Cash on Delivery</p>
+                <p className="font-bold mt-4 text-center max-md:text-sm">Cash on Delivery</p>
 
-                <div className="flex flex-col mt-10">
+                <div className="flex flex-col mt-10 max-md:mt-5">
                     <Element title="Returns">
                         <div className="flex gap-4">
-                            <div className="w-[5px] bg-green-600"></div>
-                            <p className="text-sm">We'll replace any damaged or defective parts, including broken components and non-functional batteries, if reported within [timeframe, e.g., 30 days] of delivery. Contact us on Whatsapp for issues like this.</p>
+                            <div className="w-[5px] max-md:w-[3px] bg-green-600"></div>
+                            <p className="text-sm max-md:text-[0.8rem]">We'll replace any damaged or defective parts, including broken components and non-functional batteries, if reported within [timeframe, e.g., 30 days] of delivery. Contact us on Whatsapp for issues like this.</p>
                         </div>
                     </Element>
 
                     <Element title="Specifications">
                         <div className="flex gap-4 text-sm">
-                            <div className="w-[5px] bg-green-600"></div>
-                            <div className="flex flex-col">
-                                <h1 className="text-2xl font-bold text-center">Solar Fan</h1>
+                            <div className="w-[5px] max-md:w-[3px] bg-green-600"></div>
+                            <div className="flex flex-col max-md:text-[0.8rem]">
+                                <h1 className="text-2xl font-bold text-center max-md:text-lg">Solar Fan</h1>
                                 <p>Power: 25W</p>
                                 <p>Battery: Ternary Lithium Battery 18000mah</p>
                                 <p>Solar Panel: 9V 20W Polysilicon Panel</p>
@@ -109,8 +101,8 @@ const Product = ({img, name, price, title, lastPrice}) => {
 
                     <Element title="Shipping Details">
                     <div className="flex gap-4 text-sm">
-                            <div className="w-[5px] bg-green-600"></div>
-                            <p>Shipping is FREE all over Lebanon, you'll pay the shipping agent when they arrive (Cash on Delivery) </p>
+                            <div className="w-[5px] max-md:w-[3px] bg-green-600"></div>
+                            <p className="max-md:text-[0.8rem]">Shipping is FREE all over Lebanon, you'll pay the shipping agent when they arrive (Cash on Delivery) </p>
                         </div>
                     </Element>
                 </div>
