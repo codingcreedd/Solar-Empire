1658823c96a34d58598f51aa086f85b97a932b96
codingcreedd <moubayedmarwancoding@gmail.com>
2024-06-29 14:14:23 +0300

    Add responsivity to Deals component and OurStory component

--- src/Components/FixedLayouts/Deals.jsx
+++ src/Components/FixedLayouts/Deals.jsx
@@ -1,8 +1,8 @@
 const Deals = () => {
   return (
-    <div className=" flex justify-center w-full py-1 text-sm bg-green-600 text-center text-white font-bold">
+    <div className=" flex justify-center w-full py-1 text-sm bg-green-600 text-center text-white font-bold max-md:text-[0.8rem]">
         <p>Free Delivery All Across Lebanon</p>
-        <img src="https://i.ibb.co/6nfCCxq/lebanese-flag.jpg" alt="" border="0" className=" ml-4 w-[40px] h-[20px]"/>
+        <img src="https://i.ibb.co/6nfCCxq/lebanese-flag.jpg" alt="" border="0" className=" ml-4 w-[40px] h-[20px] max-md:w-[30px] max-md:h-[18px]"/>
     </div>
   )
 }
--- src/Components/OurStory.jsx
+++ src/Components/OurStory.jsx
@@ -7,13 +7,13 @@ const OurStory = () => {
     <div className="flex flex-col items-center">
         <Deals />
         <Nav />
-        <div className="flex-grow flex flex-col mt-40 items-center">
-            <h1 className="text-7xl font-bold mb-10">Our Story</h1>
-            <p className="w-[50%]">Growing up in Lebanon, we're all too familiar with the frustration of frequent power outages. The sweltering summer nights, spent sweating in the dark, waiting for the electricity to come back on. But it was in those moments of darkness that our idea was born. We realized that there had to be a better way - a way to harness the abundant sunlight and turn it into a reliable source of energy. And so, our solar energy business was born. We started with simple, yet game-changing solutions like solar-powered fans that kept us cool on those hot summer nights, and neon lights that lit the way when the electricity failed. Today, we're dedicated to bringing clean, sustainable energy to our community, one solar panel at a time.</p>
-            <div className="flex items-center gap-6 justify-center mt-10">
-                <img src="https://i.ibb.co/HVhkfXR/Whats-App-Image-2024-06-22-at-1-25-33-PM.jpg" alt="Whats-App-Image-2024-06-22-at-1-25-33-PM" border="0" className="w-[20%] rounded-xl" />
-                <div className="w-[2rem] h-[2rem] rounded-full bg-green-600"></div>
-                <img src="https://i.ibb.co/8PQLmGn/Whats-App-Image-2024-06-22-at-10-42-39-PM.jpg" alt="Whats-App-Image-2024-06-22-at-10-42-39-PM" border="0" className="w-[20%] rounded-xl"/>
+        <div className="flex-grow flex flex-col mt-40 max-md:mt-24 items-center">
+            <h1 className="text-7xl font-bold mb-10 max-md:text-4xl">Our Story</h1>
+            <p className="w-[50%] max-md:w-[75%]">Growing up in Lebanon, we're all too familiar with the frustration of frequent power outages. The sweltering summer nights, spent sweating in the dark, waiting for the electricity to come back on. But it was in those moments of darkness that our idea was born. We realized that there had to be a better way - a way to harness the abundant sunlight and turn it into a reliable source of energy. And so, our solar energy business was born. We started with simple, yet game-changing solutions like solar-powered fans that kept us cool on those hot summer nights, and neon lights that lit the way when the electricity failed. Today, we're dedicated to bringing clean, sustainable energy to our community, one solar panel at a time.</p>
+            <div className="flex items-center gap-6 justify-center mt-10 max-md:flex-col max-md:mx-14">
+                <img src="https://i.ibb.co/HVhkfXR/Whats-App-Image-2024-06-22-at-1-25-33-PM.jpg" alt="Whats-App-Image-2024-06-22-at-1-25-33-PM" border="0" className="w-[20%] rounded-xl max-md:w-full" />
+                <div className="w-[2rem] h-[2rem] max-md:w-[1rem] max-md:h-[1rem] rounded-full bg-green-600"></div>
+                <img src="https://i.ibb.co/8PQLmGn/Whats-App-Image-2024-06-22-at-10-42-39-PM.jpg" alt="Whats-App-Image-2024-06-22-at-10-42-39-PM" border="0" className="w-[20%] rounded-xl max-md:w-full"/>
             </div>
         </div>
         <Footer />
